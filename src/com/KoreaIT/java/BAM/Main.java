0d16767aa1d40e14012ebce72532351b2a9b58fe
TaekMinJeong <http-@daum.net>
2023-05-11 19:19:45 +0900

    게시물 리스트 기능 구현

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -30,7 +30,18 @@ public class Main {
 			}
 			
 			if(cmd.equals("article list")){
-				System.out.println("게시물이 없습니다");
+				if(articles.size() == 0) {
+					continue;
+				}
+				
+				System.out.println("번호	|	제목");
+				
+				for(int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+				
 			}else if(cmd.equals("article write")){
 				System.out.println("제목 : ");
 				String title = sc.nextLine();
@@ -40,6 +51,8 @@ public class Main {
 				lastArticleID++;
 				
 				Article article = new Article(lastArticleID, title, body);
+				
+				articles.add(article);
 				
 				System.out.printf("%d번 글이 생성되었습니다\n", lastArticleID);
 				
